11e3451bbb8905682b768d9e035789b3bf9b5648
YourLocalSunny <fartravel63@gmail.com>
2026-05-27 14:54:21 -0300

    Implement 64-bit operations in bit.c
    Copilot is thinking...

--- lua/src/bit.c
+++ lua/src/bit.c
@@ -33,12 +33,17 @@
 #include "lauxlib.h"
 
 #ifdef _MSC_VER
-/* MSVC is stuck in the last century and doesn't have C99's stdint.h. */
 typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
+
+typedef signed __int64 SBits64;
+typedef unsigned __int64 UBits64;
+
 typedef unsigned __int64 uint64_t;
 #else
 #include <stdint.h>
+typedef int64_t SBits64;
+typedef uint64_t UBits64;
 #endif
 
 typedef int32_t SBits;
@@ -52,6 +57,11 @@ typedef union {
   UBits b;
 #endif
 } BitNum;
+
+typedef union {
+    lua_Number n;
+    uint64_t b;
+} BitNum64;
 
 /* Convert argument to bit type. */
 static UBits barg(lua_State *L, int idx)
@@ -90,11 +100,33 @@ static UBits barg(lua_State *L, int idx)
   return b;
 }
 
+static UBits64 barg64(lua_State *L, int idx)
+{
+#if LUA_VERSION_NUM < 502
+    lua_Number n = lua_tonumber(L, idx);
+#else
+    lua_Number n = luaL_checknumber(L, idx);
+#endif
+
+    return (UBits64)n;
+}
+
 /* Return bit type. */
 #define BRET(b)  lua_pushnumber(L, (lua_Number)(SBits)(b)); return 1;
+#define BRET64(b) \
+    lua_pushnumber(L, (lua_Number)(SBits64)(b)); \
+    return 1;
 
 static int bit_tobit(lua_State *L) { BRET(barg(L, 1)) }
+static int bit64_tobit(lua_State *L)
+{
+    BRET64(barg64(L, 1))
+}
 static int bit_bnot(lua_State *L) { BRET(~barg(L, 1)) }
+static int bit64_bnot(lua_State *L)
+{
+    BRET64(~barg64(L, 1))
+}
 
 #define BIT_OP(func, opr) \
   static int func(lua_State *L) { int i; UBits b = barg(L, 1); \
@@ -117,11 +149,61 @@ BIT_SH(bit_arshift, bsar)
 BIT_SH(bit_rol, brol)
 BIT_SH(bit_ror, bror)
 
+  #define BIT64_OP(func, opr) \
+static int func(lua_State *L) { \
+    int i; \
+    UBits64 b = barg64(L, 1); \
+    for (i = lua_gettop(L); i > 1; i--) \
+        b opr barg64(L, i); \
+    BRET64(b) \
+}
+
+BIT64_OP(bit64_band, &=)
+BIT64_OP(bit64_bor, |=)
+BIT64_OP(bit64_bxor, ^=)
+  
+#define bshl64(b, n) ((b) << (n))
+#define bshr64(b, n) ((b) >> (n))
+#define bsar64(b, n) ((SBits64)(b) >> (n))
+
+#define brol64(b, n) (((b) << (n)) | ((b) >> (64 - (n))))
+#define bror64(b, n) (((b) << (64 - (n))) | ((b) >> (n)))
+
+#define BIT64_SH(func, fn) \
+static int func(lua_State *L) { \
+    UBits64 b = barg64(L, 1); \
+    UBits64 n = barg64(L, 2) & 63; \
+    BRET64(fn(b, n)) \
+}
+
+BIT64_SH(bit64_lshift, bshl64)
+BIT64_SH(bit64_rshift, bshr64)
+BIT64_SH(bit64_arshift, bsar64)
+BIT64_SH(bit64_rol, brol64)
+BIT64_SH(bit64_ror, bror64)
+  
 static int bit_bswap(lua_State *L)
 {
   UBits b = barg(L, 1);
   b = (b >> 24) | ((b >> 8) & 0xff00) | ((b & 0xff00) << 8) | (b << 24);
   BRET(b)
+}
+
+static int bit64_bswap(lua_State *L)
+{
+    UBits64 b = barg64(L, 1);
+
+    b =
+        ((b & 0x00000000000000FFULL) << 56) |
+        ((b & 0x000000000000FF00ULL) << 40) |
+        ((b & 0x0000000000FF0000ULL) << 24) |
+        ((b & 0x00000000FF000000ULL) << 8 ) |
+        ((b & 0x000000FF00000000ULL) >> 8 ) |
+        ((b & 0x0000FF0000000000ULL) >> 24) |
+        ((b & 0x00FF000000000000ULL) >> 40) |
+        ((b & 0xFF00000000000000ULL) >> 56);
+
+    BRET64(b)
 }
 
 static int bit_tohex(lua_State *L)
@@ -138,6 +220,56 @@ static int bit_tohex(lua_State *L)
   return 1;
 }
 
+static int bit64_tohex(lua_State *L)
+{
+    UBits64 b = barg64(L, 1);
+
+    const char *hex = "0123456789abcdef";
+
+    char buf[16];
+    int i;
+
+    for (i = 15; i >= 0; --i) {
+        buf[i] = hex[b & 0xF];
+        b >>= 4;
+    }
+
+    lua_pushlstring(L, buf, 16);
+    return 1;
+}
+
+static int bit_tobin(lua_State *L)
+{
+    UBits b = barg(L, 1);
+
+    char buf[33];
+    int i;
+
+    for (i = 31; i >= 0; --i)
+        buf[31 - i] = (b & (1u << i)) ? '1' : '0';
+
+    buf[32] = '\0';
+
+    lua_pushstring(L, buf);
+    return 1;
+}
+
+static int bit64_tobin(lua_State *L)
+{
+    UBits64 b = barg64(L, 1);
+
+    char buf[65];
+    int i;
+
+    for (i = 63; i >= 0; --i)
+        buf[63 - i] = (b & ((UBits64)1 << i)) ? '1' : '0';
+
+    buf[64] = '\0';
+
+    lua_pushstring(L, buf);
+    return 1;
+}
+
 static const struct luaL_Reg bit_funcs[] = {
   { "tobit",	bit_tobit },
   { "bnot",	bit_bnot },
@@ -151,7 +283,25 @@ static const struct luaL_Reg bit_funcs[] = {
   { "ror",	bit_ror },
   { "bswap",	bit_bswap },
   { "tohex",	bit_tohex },
+  { "tobin",  bit_tobin },
   { NULL, NULL }
+};
+
+static const struct luaL_Reg bit64_funcs[] = {
+    { "tobit", bit64_tobit },
+    { "bnot", bit64_bnot },
+    { "band", bit64_band },
+    { "bor", bit64_bor },
+    { "bxor", bit64_bxor },
+    { "lshift", bit64_lshift },
+    { "rshift", bit64_rshift },
+    { "arshift", bit64_arshift },
+    { "rol", bit64_rol },
+    { "ror", bit64_ror },
+    { "bswap", bit64_bswap },
+    { "tohex", bit64_tohex },
+    { "tobin", bit64_tobin },
+    { NULL, NULL }
 };
 
 /* Signed right-shifts are implementation-defined per C89/C99.
@@ -185,5 +335,16 @@ LUALIB_API int luaopen_bit(lua_State *L)
   luaL_newlib(L, bit_funcs);
 #endif
   return 1;
+}
+
+LUALIB_API int luaopen_bit64(lua_State *L)
+{
+#if LUA_VERSION_NUM < 502
+    luaL_register(L, "bit64", bit64_funcs);
+#else
+    luaL_newlib(L, bit64_funcs);
+#endif
+
+    return 1;
 }
 
